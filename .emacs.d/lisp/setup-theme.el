48bfcee58c090a3df8b1343731c4483743f7821e
Nikolay Golikov <decaprox@gmail.com>
2024-06-17 09:35:08 +0400

    emacs: theme tuning

--- .emacs.d/lisp/setup-theme.el
+++ .emacs.d/lisp/setup-theme.el
@@ -9,7 +9,10 @@
                (load-theme 'ample-light t t)
                (enable-theme 'ample))
   :defer t
-  :ensure t)
+  :ensure t
+  :config (progn (set-face-background 'hl-line "#1f2527")
+                 (set-face-foreground 'hl-line nil)
+                 (set-face-background 'region "#666666")))
 
 (provide 'setup-theme)
 
